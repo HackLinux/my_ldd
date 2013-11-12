8af16dea5ee71cf0b344c684fd90ecc864b83fde
latelee <latelee@163.com>
2013-11-12 21:14:01 +0800

    添加platform驱动示例。
    Signed-off-by: latelee <latelee@163.com>

---
+++ platform_char/simplechar.h
@@ -0,0 +1,8 @@
+#ifndef SIMPLECHAR_H
+#define SIMPLECHAR_H
+
+struct simplechar_platdata {
+    char    *name;
+};
+
+#endif
---
+++ platform_char/simplechar_dev.c
@@ -0,0 +1,37 @@
+/**
+ * @file   simplechar_dev.c
+ * @author Late Lee <latelee@163.com>
+ * @date   Tue Nov 12 21:07:03 2013
+ * 
+ * @brief  
+ * 
+ * 
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+
+#include "simplechar.h"
+
+// device
+static struct simplechar_platdata foo_pdata = {
+    .name        = "gotohell",
+};
+
+// 避免删掉模块时出现警告
+static void simplechar_dev_release(struct device* dev)
+{
+    printk(KERN_NOTICE "do %s case of: Device xxx does not have a release() function, it is broken and must be fixed.\n", __func__);
+    return;
+}
+
+// 另一文件要使用到，此处不能为static
+struct platform_device simplechar_device = {
+    .name    = "simplechar",
+    .id      = 1,
+    .dev     = {
+        .platform_data = &foo_pdata,
+        .release = &simplechar_dev_release,
+    },
+};
